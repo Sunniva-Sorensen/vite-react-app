92cffa85633488a6d7ae081fa3d9f6c2a4b16ab5
Sunniva Sørensen <sunnsor@gmail.com>
2026-09-17 14:20:19 +0200

    article link only on headers

--- src/components/ArticlePreview/ArticlePreview.tsx
+++ src/components/ArticlePreview/ArticlePreview.tsx
@@ -12,21 +12,25 @@ export function ArticlePreview({ article, onFavourite }: ArticlePreviewProps) {
   const { isLoggedIn } = useAuthContext();
 
   return (
-    <Link to={{ pathname: `/article/${article.slug}` }} state={{ article }}
-      className="article-link">
-      <div className="article-preview">
+    <div className="article-preview">
+      <Link
+        to={{ pathname: `/article/${article.slug}` }}
+        state={{ article }}
+        className="article-link"
+      >
         <h2>{article.title}</h2>
-        <p>{article.description}</p>
-        <p>By {article.author.username}</p>
+      </Link>
 
-        <button
-          className={`favouriteButton ${article.favorited ? "favorited" : ""}`}
-          onClick={onFavourite}
-          disabled={!isLoggedIn}
-        >
-          {article.favorited ? "Unfavourite" : "Favourite"}
-        </button>
-      </div>
-    </Link>
+      <p>{article.description}</p>
+      <p>By {article.author.username}</p>
+
+      <button
+        className={`favouriteButton ${article.favorited ? "favorited" : ""}`}
+        onClick={onFavourite}
+        disabled={!isLoggedIn}
+      >
+        {article.favorited ? "Unfavourite" : "Favourite"}
+      </button>
+    </div>
   );
 }
